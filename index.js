b9b0ca760ab0f807585035d01118f8c107feaf27
terryblue99 <terryblue@comcast.net>
2018-03-08 17:40:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 function getIssues() {
+  console.log("*** getIssues")
 }
 
 function showIssues(json) {
+  console.log("*** showIssues")
 }
 
 function createIssue() {
+  console.log("*** createIssue")
+  const title = document.getElementById("title").value
+  const body = document.getElementById("body").value
+  const repo = document.getElementById("results").innerHTML
+  console.log("*** createIssue repo:", repo)
 }
 
 function showForkedRepo(json) {
-  console.log("*** json.full_name: ", json.full_name)
   console.log("*** json: ", json)
-  const repo = `<p><li><a href="https://github.com/${json.full_name}" target="_blank">Get Repo</a></p>`
+  const repo = `<p><a href="https://github.com/${json.full_name}" target="_blank">Get Repo</a></p>`
   document.getElementById("results").innerHTML = repo
 }
 
 function forkRepo() {
   const token = getToken()
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  //use fetch to fork the repository
   fetch(`https://api.github.com/repos/${repo}/forks`, {
     method: 'POST',
     headers: {
@@ -40,5 +46,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '75520947b97932e7625de06a49f2b2c8b9fa19c5'
+  return '535db158c925e9dd44e3d7f3a68dbe836a24bf6c'
 }
