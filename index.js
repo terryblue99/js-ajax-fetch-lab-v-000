94109c9b5df90cfc70300f501bd1cc3daf82f082
terryblue99 <terryblue@comcast.net>
2018-03-08 17:57:47 -0600

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ function createIssue() {
   console.log("*** createIssue")
   const title = document.getElementById("title").value
   const body = document.getElementById("body").value
-  const repo = document.getElementById("results").innerHTML
-  console.log("*** createIssue repo:", repo)
+  const ownerRepo = document.getElementById('ownerRepo').value
+  console.log("*** createIssue ownerRepo:", ownerRepo)
 }
 
 function showForkedRepo(json) {
   console.log("*** json: ", json)
+  document.getElementById('ownerRepo').value = json.full_name
   const repo = `<p><a href="https://github.com/${json.full_name}" target="_blank">Get Repo</a></p>`
   document.getElementById("results").innerHTML = repo
 }
@@ -46,5 +47,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '535db158c925e9dd44e3d7f3a68dbe836a24bf6c'
+  return '8b4992e8e35f498ec259b292f381c6ce5fbd1b43'
 }
