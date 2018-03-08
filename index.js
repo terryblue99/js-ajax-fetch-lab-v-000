ac7ef94b2310faf3c1935c763346f011c20bb2fe
Terry Blue <terryblue@comcast.net>
2018-03-08 04:21:33 +0000

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ function createIssue() {
 function showResults(json) {
 }
 
+
 function forkRepo() {
+  const token = getToken()
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+    Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res))
+
 }
 
 function getToken() {
